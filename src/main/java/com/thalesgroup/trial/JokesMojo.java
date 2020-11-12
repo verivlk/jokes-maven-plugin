22a2f44e8cd9057e75810a3711f2cdcdee867d2f
Veri <verivlk@gmail.com>
2020-11-12 17:13:51 +0100

    Mojo class and pom configuration

---
+++ src/main/java/com/thalesgroup/trial/JokesMojo.java
@@ -0,0 +1,12 @@
+package com.thalesgroup.trial;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class JokesMojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
